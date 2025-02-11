cb57a7354dd86f1c8ee31ca1b219c63d70ba44a3
Luke Di Bert <luke.di-bert@stu.ocsb.ca>
2025-02-11 17:52:22 +0000

    added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created By: Luke
+# Date: Feb 11, 2025
+# Write Display a message using proper format and style
+
+
+def main():
+    print("Hello, this is using proper style")
+    print("I'm testing my function")
+
+
+if __name__ == "__main__":
+    main()
